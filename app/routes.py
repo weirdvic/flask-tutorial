531759a921ef88f40a4c56f09db046af9073bb27
weirdvic <56339132+weirdvic@users.noreply.github.com>
2022-07-22 17:22:46 +0300

    Update templates

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
 
@@ -29,5 +29,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('login'))
     return render_template('login.html', title='Sign In', form=form)
