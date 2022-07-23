c913e5f6525a2b7c931de0750f87c3644229e830
weirdvic <56339132+weirdvic@users.noreply.github.com>
2022-07-23 09:40:37 +0300

    Implement login

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,14 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.urls import url_parse
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Эльдар Рязанов'}
     posts = [
@@ -26,8 +30,25 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Если текущий пользователь уже залогинился, отправить его на главную
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))   
+    # В противном случае пытаемся залогиниться
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect(url_for('login'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
