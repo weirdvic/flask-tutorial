b5fe49231d4d669103936ae375ee1178e5b85505
weirdvic <56339132+weirdvic@users.noreply.github.com>
2022-07-26 15:45:51 +0300

    Timestamps in local timezone

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -18,6 +19,8 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+moment = Moment(app)
+
 
 from app import routes, models, errors
 
